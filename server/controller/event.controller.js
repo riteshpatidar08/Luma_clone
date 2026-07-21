dc301ff5b54ab5ad97a2154298aebca8cfc65fb1
ritesh <riteshpatidar088@gmail.com>
2026-07-21 11:55:00 +0530

    added delete event and get Event

--- server/controller/event.controller.js
+++ server/controller/event.controller.js
@@ -40,55 +40,70 @@ export const createEvent = async (req, res) => {
 
 export const getEvents = async (req, res) => {
   try {
-    const { limit, page, sort ,searchQuery } = req.query;
+    const { limit, page, sort, searchQuery } = req.query;
 
     let sortValue;
     if (sort === 'asc') sortValue = 1;
     if (sort === 'desc') sortValue = -1;
-const filter = {} ;
-if(searchQuery) filter.$text = {$search : searchQuery}
+    const filter = {};
+    if (searchQuery) filter.$text = { $search: searchQuery };
     const events = await Event.find(filter)
       .sort({ 'schedule.startDate': sortValue || 1 })
       .skip((page - 1) * Number(limit))
       .limit(Number(limit));
     const totalEventDocument = await Event.countDocuments();
- 
+
     res.json({
       totalEvents: totalEventDocument,
       length: events.length,
       data: events,
     });
   } catch (error) {
-    res.send(error.message)
+    res.send(error.message);
   }
 };
-
-
-
-
 
 export const getEventsById = async (req, res) => {
   try {
-    const { id } = req.params
+    const { id } = req.params;
 
-  
-    const events = await Event.findById(id)
-     
-    if(!events){
+    const events = await Event.findById(id);
+
+    if (!events) {
       return res.status(404).json({
-        message : "No event found"
-      })
+        message: 'No event found',
+      });
     }
- 
+
     res.json({
-      message : "fetched successfully",
-     success : true ,
+      message: 'fetched successfully',
+      success: true,
       data: events,
     });
   } catch (error) {
-    res.send(error.message)
+    res.send(error.message);
   }
 };
 
-
 //delete event and update event
+export const deleteEvent = async (req, res) => {
+  try {
+    const { id } = req.params;
+
+    const events = await Event.findByIdAndDelete(id);
+
+    // if (!events) {
+    //   return res.status(404).json({
+    //     message: 'No event found',
+    //   });
+    // }
+
+    res.status(204).json({
+      message: 'Delete Successfullu',
+      success: true,
+      data: events,
+    });
+  } catch (error) {
+    res.send(error.message);
+  }
+};
