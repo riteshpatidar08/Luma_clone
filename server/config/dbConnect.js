90348e7b34ef95b174e6d7915fcba347007e26b6
ritesh <riteshpatidar088@gmail.com>
2026-07-29 12:46:17 +0530

    fix:timeout

--- server/config/dbConnect.js
+++ server/config/dbConnect.js
@@ -4,6 +4,8 @@ import {mongoose} from 'mongoose'
 export const dbConnect = async() => {
     try {
         const connection = await mongoose.connect(process.env.MONGODB_URI ,  {defaultMaxTimeMS: 30000 })// 10 seconds default)
+        mongoose.set('bufferCommands', false);
+mongoose.set('bufferMaxEntries', 0);
         console.log('connection successfull')
     } catch (error) {
         console.log(error.message)
