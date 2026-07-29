0bcb193c6b476c9354d8a5c3c202fe44c26ee517
ritesh <riteshpatidar088@gmail.com>
2026-07-29 12:26:21 +0530

    fix:keys

--- server/app.js
+++ server/app.js
@@ -11,9 +11,22 @@ import eventRouter from './routes/event.routes.js';
 import cors from 'cors';
 import admin from 'firebase-admin'
 
-import serviceAccount from './key/test-1bf72-firebase-adminsdk-pg6k8-a8e3134601.json' with {type : 'json'} ;
+// import serviceAccount from './key/test-1bf72-firebase-adminsdk-pg6k8-a8e3134601.json' with {type : 'json'} ;
 
 const app = express();
+
+const serviceAccount = {
+  type: process.env.FIREBASE_TYPE,
+  project_id: process.env.FIREBASE_PROJECT_ID,
+  private_key_id: process.env.FIREBASE_PRIVATE_KEY_ID,
+  private_key: process.env.FIREBASE_PRIVATE_KEY,
+  client_email: process.env.FIREBASE_CLIENT_EMAIL,
+  client_id: process.env.FIREBASE_CLIENT_ID,
+  auth_uri: process.env.FIREBASE_AUTH_URI,
+  token_uri: process.env.FIREBASE_TOKEN_URI,
+  auth_provider_x509_cert_url: process.env.FIREBASE_AUTH_PROVIDER_X509_CERT_URL,
+  client_x509_cert_url: process.env.FIREBASE_CLIENT_X509_CERT_URL
+};
 
 console.log(admin)
 admin.initializeApp({
@@ -28,6 +41,10 @@ app.use(cors());
 app.use(express.json());
 dotenv.config(); //configure the .env variables to use in the app
 dbConnect(); //make connection with databse :
+
+app.get('/' , (req,res)=>{
+  res.send("Server deployed....")
+})
 
 app.use('/api/v1', authRouter); //authentication logic
 app.use('/api/v1', eventRouter); //event logic 
