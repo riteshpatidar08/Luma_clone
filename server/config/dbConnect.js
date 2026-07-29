151e7bf4134bf6781dfd626fbc6e4811c68a7a5b
ritesh <riteshpatidar088@gmail.com>
2026-07-29 12:52:50 +0530

    fix:buffer

--- server/config/dbConnect.js
+++ server/config/dbConnect.js
@@ -1,13 +1,14 @@
+import { mongoose } from 'mongoose';
 
-import {mongoose} from 'mongoose'
-
-export const dbConnect = async() => {
-    try {
-        const connection = await mongoose.connect(process.env.MONGODB_URI ,  {defaultMaxTimeMS: 30000 })// 10 seconds default)
-        mongoose.set('bufferCommands', false);
-mongoose.set('bufferMaxEntries', 0);
-        console.log('connection successfull')
-    } catch (error) {
-        console.log(error.message)
-    }
-}
+export const dbConnect = async () => {
+  try {
+    const connection = await mongoose.connect(process.env.MONGODB_URI, {
+        serverSelectionTimeoutMS: 60000, // 60 seconds
+    }); // 10 seconds default)
+    mongoose.set('bufferCommands', false);
+    // mongoose.set('bufferMaxEntries', 0);
+    console.log('connection successfull');
+  } catch (error) {
+    console.log(error.message);
+  }
+};
